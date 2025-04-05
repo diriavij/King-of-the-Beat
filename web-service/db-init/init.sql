db9540c469d5a6c34282af0516ff45b07f39cc03
diriavij <popovfoma06@gmail.com>
2025-04-05 23:31:28 +0300

    Voting screen added

--- web-service/db-init/init.sql
+++ web-service/db-init/init.sql
@@ -1,4 +1,4 @@
--- Создание таблицы пользователей
+-- Таблица пользователей
 CREATE TABLE IF NOT EXISTS "user" (
     user_id SERIAL PRIMARY KEY,
     balance INT,
@@ -7,18 +7,18 @@ CREATE TABLE IF NOT EXISTS "user" (
     history_id SERIAL
 );
 
--- Создание таблицы комнат
+-- Таблица комнат
 CREATE TABLE IF NOT EXISTS "room" (
     room_id SERIAL PRIMARY KEY,
-    owner_id SERIAL,
+    owner_id INTEGER,
     name VARCHAR,
     topic VARCHAR
 );
 
--- Создание таблицы участия
+-- Таблица участия
 CREATE TABLE IF NOT EXISTS "participation" (
-    user_id SERIAL NOT NULL,
-    room_id SERIAL NOT NULL,
+    user_id INTEGER NOT NULL,
+    room_id INTEGER NOT NULL,
     PRIMARY KEY (user_id, room_id),
     FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE,
     FOREIGN KEY (room_id) REFERENCES "room"(room_id) ON DELETE CASCADE
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS "participation" (
 ALTER TABLE "participation" ADD COLUMN is_submitted BOOLEAN DEFAULT FALSE;
 ALTER TABLE "participation" ADD COLUMN bets_submitted BOOLEAN DEFAULT FALSE;
 
--- Создание таблицы песен
+-- Таблица песен
 CREATE TABLE IF NOT EXISTS "song" (
   song_id SERIAL PRIMARY KEY,
   room_id INTEGER NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS "song" (
   FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE
 );
 
--- Создание таблицы ставок
+-- Таблица ставок
 CREATE TABLE IF NOT EXISTS "bets" (
     bet_id SERIAL PRIMARY KEY,
     room_id INTEGER NOT NULL,
@@ -47,6 +47,15 @@ CREATE TABLE IF NOT EXISTS "bets" (
     song_id INTEGER NOT NULL,
     bet_amount INTEGER,
     FOREIGN KEY (room_id) REFERENCES "room"(room_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (song_id) REFERENCES "song"(song_id) ON DELETE CASCADE
+);
+
+-- Таблица голосования (голосования за песни)
+CREATE TABLE IF NOT EXISTS "votes" (
+    vote_id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    song_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE,
     FOREIGN KEY (song_id) REFERENCES "song"(song_id) ON DELETE CASCADE
 );
